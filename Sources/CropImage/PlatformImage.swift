374553281484146dbb77e749262b02198fdf4c6e
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2024-03-19 01:10:19 +0000

    update docs

--- Sources/CropImage/PlatformImage.swift
+++ Sources/CropImage/PlatformImage.swift
@@ -11,12 +11,12 @@ import Foundation
 import AppKit
 /// The image object type, aliased to each platform.
 ///
-/// On macOS, it's `NSImage` and on iOS it's `UIImage`.
+/// On macOS, it's `NSImage` and on iOS/visionOS it's `UIImage`.
 public typealias PlatformImage = NSImage
 #else
 import UIKit
 /// The image object type, aliased to each platform.
 ///
-/// On macOS, it's `NSImage` and on iOS it's `UIImage`.
+/// On macOS, it's `NSImage` and on iOS/visionOS it's `UIImage`.
 public typealias PlatformImage = UIImage
 #endif
